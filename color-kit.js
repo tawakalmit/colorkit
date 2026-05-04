2f20d9a9ad7c0bd9a5b8106ffcc1820d61ac6069
tawakalmit <tawakal.mit@gmail.com>
2026-05-05 06:35:54 +0700

    first commit

---
+++ color-kit.js
@@ -0,0 +1,15 @@
+function hexToRgba(hex, alpha = 1) {
+    // hapus tanda #
+    hex = hex.replace('#', '');
+
+    // support shorthand (#fff)
+    if (hex.length === 3) {
+        hex = hex.split('').map(char => char + char).join('');
+    }
+
+    const r = parseInt(hex.substring(0, 2), 16);
+    const g = parseInt(hex.substring(2, 4), 16);
+    const b = parseInt(hex.substring(4, 6), 16);
+
+    return `rgba(${r}, ${g}, ${b}, ${alpha})`;
+}
